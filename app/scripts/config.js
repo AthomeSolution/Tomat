c99a9a63d64459b33666d34b94df89819736d628
Axelle Ziegler <axelle.ziegler@athome-solution.fr>
2014-06-20 08:58:28 +0200

    config

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -3,9 +3,7 @@
 
     angular.module('backendInterfaceApp')
         .value('config', {
-            //url: 'http://toolbox.baasbox.demos.athome-solution.fr',
-           // url: 'http://localhost:9000',
-            url: 'http://dev.athome-solution.fr:9001',
+            url: 'http://localhost:9000',
             appcode: '1234567890',
             instagramClientId: 'd9a1295c86da4fdf86f4498fb04c4b10',
             useInstagram:false
