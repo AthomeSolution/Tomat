b7f696e10828ea15b1995cea0c54e2275e21c2e8
hbenmansour <hatem.benmansour@athome-solution.fr>
2014-09-12 17:24:57 +0200

    delete field datatypes

--- interface/app/scripts/config.js
+++ interface/app/scripts/config.js
@@ -3,7 +3,9 @@
 
     angular.module('backendInterfaceApp')
         .value('config', {
-            url: '..',
+            //url: 'http://appli.canal-du-nivernais.com:9000',
+            url: 'http://localhost:9000',
+            //url: '..',
             appcode: '1234567890',
             instagramClientId: 'd9a1295c86da4fdf86f4498fb04c4b10',
             useInstagram:false
--- interface/app/scripts/controllers/datatype.js
+++ interface/app/scripts/controllers/datatype.js
@@ -3,19 +3,27 @@ angular.module('backendInterfaceApp')
     function ($scope, $location, $window, auth, baasbox, config) {
         var init = function () {
 
-            baasbox.listDocuments("datatypes").then(function(data){
+            baasbox.listDocuments("datatypes").then(function (data) {
                 $scope.datatypes = data;
             });
+
 
         }
 
         $scope.addNewField = function () {
             $scope.content.selectedDatatype.structure.push({
-                name:"New Field"
+                name: "New Field"
             });
         }
+        $scope.deleteField = function (index) {
 
-        $scope.deleteItem = function(field) {
+            var deleteField = $window.confirm('Are you absolutely sure you want to delete this field?');
+            if (deleteField) {
+                $scope.content.selectedDatatype.structure.splice(index, 1);
+                $scope.save();
+            }
+        }
+        $scope.deleteItem = function (field) {
             var deleteUser = $window.confirm('Are you absolutely sure you want to delete?');
 
             if (deleteUser) {
@@ -24,48 +32,49 @@ angular.module('backendInterfaceApp')
 
         }
 
-        $scope.delete = function(index) {
-            $scope.content.selectedDatatype.structure.splice(index,1);
+        $scope.delete = function (index) {
+            $scope.content.selectedDatatype.structure.splice(index, 1);
         }
 
-        $scope.createNewType = function()  {
-            $scope.content.selectedDatatype ={};
-            $scope.content.selectedDatatype.name="Name";
+        $scope.createNewType = function () {
+            $scope.content.selectedDatatype = {};
+            $scope.content.selectedDatatype.name = "Name";
             $scope.content.selectedDatatype.structure = [];
         }
 
-        $scope.duplicate = function() {
+        $scope.duplicate = function () {
             var dataType = $scope.content.selectedDatatype;
-            $scope.content.selectedDatatype ={};
-            $scope.content.selectedDatatype.name="Name";
+            $scope.content.selectedDatatype = {};
+            $scope.content.selectedDatatype.name = "Name";
             $scope.content.selectedDatatype.structure = [];
             for (var i = 0; i < dataType.structure.length; i++) {
                 var field = dataType.structure[i];
                 $scope.content.selectedDatatype.structure.push({
-                    name:field.name,
-                    path:field.path,
-                    type:field.type
+                    name: field.name,
+                    path: field.path,
+                    type: field.type
                 });
             }
         }
-        $scope.save = function(){
-            baasbox.createOrUpdateDocument("datatypes",$scope.content.selectedDatatype).then(
-                function(data){
+        $scope.save = function () {
+
+            baasbox.createOrUpdateDocument("datatypes", $scope.content.selectedDatatype).then(
+                function (data) {
                     $scope.content.selectedDatatype = data.data;
                     init();
                 }
             )
         }
 
-        $scope.downloadXML = function(){
-           baasbox.downloadXML( "poi","").then(function(response){
+        $scope.downloadXML = function () {
+            baasbox.downloadXML("poi", "").then(function (response) {
 
-                var blob = new Blob([ response ], { type : 'text/plain' });
-                window.open((window.URL || window.webkitURL).createObjectURL( blob ), '_self');
-           });
+                var blob = new Blob([ response ], { type: 'text/plain' });
+                window.open((window.URL || window.webkitURL).createObjectURL(blob), '_self');
+            });
         }
 
-        $scope.content={};
-        $scope.content.selectedDatatype ={};
+        $scope.content = {};
+        $scope.content.selectedDatatype = {};
         init();
-});
+    });
