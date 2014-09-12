2e04e6b69f9f3763f3e4e30013a891a2f2c8d1eb
hbenmansour <hatem.benmansour@athome-solution.fr>
2014-09-12 17:25:43 +0200

    config

--- interface/app/scripts/config.js
+++ interface/app/scripts/config.js
@@ -3,8 +3,8 @@
 
     angular.module('backendInterfaceApp')
         .value('config', {
-            //url: 'http://appli.canal-du-nivernais.com:9000',
-            url: 'http://localhost:9000',
+            url: 'http://appli.canal-du-nivernais.com:9000',
+            //url: 'http://localhost:9000',
             //url: '..',
             appcode: '1234567890',
             instagramClientId: 'd9a1295c86da4fdf86f4498fb04c4b10',
